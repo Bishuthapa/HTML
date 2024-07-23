2b683f42ba172aeff847949d9f1a90188f8071d4
Bishu Thapa <thapabishu435@gmail.com>
2024-07-23 09:39:18 +0545

    html and css

---
+++ javascript.js
@@ -0,0 +1,25 @@
+/*let string ="Bishu";
+let size=0;
+for(let i of str)
+{
+    console.log("i:", i);
+    size++;
+}
+console.log("String size: ", size);
+let str = "BISHU";
+for(let i of str)
+{
+    console.log("i : ", i);
+    size++;
+}
+console.log("string size is:" ,size);
+/*
+let gameNum =25;
+let userNum = prompt("Guess the game number:");
+while(userNum!=gameNum)
+{
+    userNum=prompt("You enter tge wrong number guess the game number:");
+}
+
+console.log("Congrulation, you guessed the correct game number.");
+console.log("good boy.");*/
